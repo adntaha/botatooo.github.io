393cb9f0452b4e86b426807eddfb231325fcc11d
botatooo <botatostuff@gmail.com>
2022-06-02 23:09:26 -0400

    🎉 Initial commit

--- compile.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-SETLOCAL
-
-CD /D %~dp0
-sass src\style.scss style.css --no-source-map
-
-ENDLOCAL
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<!--
+Copyright © 2022-present Adnan-Aidan Taha
+This file is part of "botato.gq".
+
+"botato.gq" is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+"botato.gq" is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with "botato.gq".
+If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'IndexPage',
+});
+</script>
